1d42db35d54a57a9e1e085d79930dc4a753a2785
IsmNarmin13 <narminvi@code.edu.az>
2024-04-17 17:54:55 +0400

    first commit

---
+++ riode_narmin/Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace riode_narmin.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ riode_narmin/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RiodeDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+    );
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+app.Run();
